2e4681cabf380dbf5adc21014b2854870c620894
Dex-store <bfars725@gmail.com>
2026-01-11 20:13:12 +0200

    Create config.js

---
+++ app/assets/config.js
@@ -0,0 +1,10 @@
+// assets/config.js
+window.APP_CONFIG = {
+  APP_ID: "PUT_YOUR_APP_ID_HERE",
+  JS_KEY: "PUT_YOUR_JAVASCRIPT_KEY_HERE",
+  SERVER_URL: "https://parseapi.back4app.com/",
+  RECEIVER_WALLET: "0xb56a89f790060cbb0f08d02234921e5fefff11a6",
+  TELEGRAM_SUPPORT: "https://t.me/TIK_USDT",
+  TELEGRAM_CHANNEL: "https://t.me/+P2nKCspWsyNmYmFk",
+  REPO_BASE: "" // اتركها فارغة في GitHub Pages
+};
