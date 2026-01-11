d4032ab73f19c73195ce5542522d6d01b6ca651d
Dex-store <bfars725@gmail.com>
2026-01-11 20:14:39 +0200

    Update config.js

--- app/assets/config.js
+++ app/assets/config.js
@@ -1,7 +1,7 @@
 // assets/config.js
 window.APP_CONFIG = {
-  APP_ID: "PUT_YOUR_APP_ID_HERE",
-  JS_KEY: "PUT_YOUR_JAVASCRIPT_KEY_HERE",
+  APP_ID: "8KYckDDwKPakgqNYPCZ2NcqDpGMsII3ZnMTE5RTx",
+  JS_KEY: "uSV2StUZpizr8aoGUYpBJMZyKxwtXhGstQd8HZaF",
   SERVER_URL: "https://parseapi.back4app.com/",
   RECEIVER_WALLET: "0xb56a89f790060cbb0f08d02234921e5fefff11a6",
   TELEGRAM_SUPPORT: "https://t.me/TIK_USDT",
