9d79917df06b655033bc39662c01a52d6cd8b5f7
Dex-store <bfars725@gmail.com>
2026-01-11 21:44:03 +0200

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,13 @@
+// app/config.js
+window.TIK = {
+  APP_NAME: "TIK-USDT",
+
+  // ضع Application ID من Back4App
+  PARSE_APP_ID: "8KYckDDwKPakgqNYPCZ2NcqDpGMsII3ZnMTE5RTx",
+
+  // ضع JavaScript Key من Back4App
+  PARSE_JS_KEY: "uSV2StUZpizr8aoGUYpBJMZyKxwtXhGstQd8HZaF",
+
+  // Back4App Parse Server URL (ثابت غالبًا)
+  PARSE_SERVER_URL: "https://parseapi.back4app.com/"
+};
