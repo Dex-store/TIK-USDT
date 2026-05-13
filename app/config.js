91c4524de5848da5ec5b0a93afadf2ed6f2a2dda
Dex-store <bfars725@gmail.com>
2026-05-13 13:29:04 +0200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,10 +1,7 @@
 // app/config.js
 window.TIK = {
-  APP_NAME: "GOLDX", // الاسم الجديد للمنصة
-
-  // مفاتيح الربط بـ Back4App (مؤمنة وجاهزة)
+  APP_NAME: "GOLDX",
   PARSE_APP_ID: "8KYckDDwKPakgqNYPCZ2NcqDpGMsII3ZnMTE5RTx",
   PARSE_JS_KEY: "uSV2StUZpizr8aoGUYpBJMZyKxwtXhGstQd8HZaF",
   PARSE_SERVER_URL: "https://parseapi.back4app.com/"
 };
-
