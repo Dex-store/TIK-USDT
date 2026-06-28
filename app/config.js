eb51f04225def38df159b902d32eed0153dce79f
Dex-store <bfars725@gmail.com>
2026-06-28 19:24:01 +0200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 // app/config.js
-window.TIK = {
-  APP_NAME: "GOLDX",
+window.GOLDX = {
+  APP_NAME: "GOLD-X",
   PARSE_APP_ID: "8KYckDDwKPakgqNYPCZ2NcqDpGMsII3ZnMTE5RTx",
   PARSE_JS_KEY: "uSV2StUZpizr8aoGUYpBJMZyKxwtXhGstQd8HZaF",
   PARSE_SERVER_URL: "https://parseapi.back4app.com/"
